8cca67003967b5d19ad96e324c8f6fb829704a85
Maneesh <maneeshsriram123@gmail.com>
2021-06-14 09:13:21 +0530

    Update settings.py

--- analysis/settings.py
+++ analysis/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#v-gee^(&nd59!^hjl2dh+yu&u+)x+&%f+tu-!2g&!!bmgqp1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['auto-analysis-data.herokuapp.com', '127.0.0.1']
 
